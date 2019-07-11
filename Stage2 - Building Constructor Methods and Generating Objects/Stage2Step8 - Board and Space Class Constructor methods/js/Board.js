12d6807467a1e339a7ce4a4f48bcfe1378fd28c1
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-11 15:11:53 -0400

    Board and Space class constructor methods

--- Stage2 - Building Constructor Methods and Generating Objects/Stage2Step8 - Board and Space Class Constructor methods/js/Board.js
+++ Stage2 - Building Constructor Methods and Generating Objects/Stage2Step8 - Board and Space Class Constructor methods/js/Board.js
@@ -1,3 +1,8 @@
 class Board {
-    
+  // no arguments are passed because each game will have only 1 board object and the number of rows and columns is set
+  constructor () {
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = [];
+  }
 }
