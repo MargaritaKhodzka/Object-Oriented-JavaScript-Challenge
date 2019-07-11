7da92377be1beee2dc2906994b861c76cc8c1380
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-11 14:58:56 -0400

    build createTokens method for Player class

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
@@ -4,6 +4,22 @@ class Player {
     this.name = name;
     this.color = color;
     this.active = active;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+  }
+
+  /*
+    Creates token objects for player
+    @param {integer} num - number of token objects to be created
+    @return {array} tokens - an array of new token objects
+  */
+
+  createTokens(num) {
+    const tokens = [];
+
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+    return tokens;
   }
 }
